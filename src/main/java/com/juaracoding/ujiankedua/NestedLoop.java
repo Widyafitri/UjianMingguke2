f92b185b35e3e75193560faff41798316b32aea9
widiya fitri <23widyafitri@gmail.com>
2022-10-09 23:36:16 +0700

    Ujian minggu ke dua

---
+++ src/main/java/com/juaracoding/ujiankedua/NestedLoop.java
@@ -0,0 +1,19 @@
+package com.juaracoding.ujiankedua;
+
+public class NestedLoop {
+    public static void main(String[] args) {
+        int x = 1;
+        for (int i = 9; i >= 0; i--) {
+            for (int j = x; j <= i; j++) {
+                if (j % 2 == 1) {
+                    System.out.print(j);
+                } else {
+                    System.out.print("_");
+                }
+            }
+            x++;
+            System.out.println();
+        }
+
+    }
+}
